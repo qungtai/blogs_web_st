aa803d0931e977756e70af776992e3da6796ea9d
lequangtai <lequangtai1602@gmail.com>
2024-10-25 17:01:29 +0700

    update welcome page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blogs_web_st
 tai's strreamlit app to store blogs of himself.
+click to this link: [LQT' Blog] (https://lqtblogs.streamlit.app/)
--- main.py
+++ main.py
@@ -1,10 +1,203 @@
-import streamlit as st 
+"""
 
-def main_page():
-    st.title('hello')
-    st.write('afjasdfjasdfasdjfajdlsk')
-    st.write('asdasdfasdf')
-    
-    
-if __name__ == "__main__": 
-    main_page()
+All code contributed to Exifa.net is © 2024 by Sahir Maharaj. 
+The content is licensed under the Creative Commons Attribution 4.0 International License. 
+This allows for sharing and adaptation, provided appropriate credit is given, and any changes made are indicated.
+
+When using the code from Exifa.net, please credit as follows: "Code sourced from Exifa.net, authored by Sahir Maharaj, 2024."
+
+For reporting bugs, requesting features, or further inquiries, please reach out to Sahir Maharaj at sahir@sahirmaharaj.com.
+
+Connect with Sahir Maharaj on LinkedIn for updates and potential collaborations: https://www.linkedin.com/in/sahir-maharaj/
+
+Hire Sahir Maharaj: https://topmate.io/sahirmaharaj/362667
+"""
+
+import streamlit as st
+import replicate
+import os
+import pdfplumber
+from docx import Document
+import pandas as pd
+from io import BytesIO
+from transformers import AutoTokenizer
+import exifread
+import requests
+from PIL import Image
+import numpy as np
+import plotly.express as px
+import matplotlib.colors as mcolors
+import plotly.graph_objs as go
+import streamlit.components.v1 as components
+import random
+
+config = {
+    "toImageButtonOptions": {
+        "format": "png",
+        "filename": "custom_image",
+        "height": 720,
+        "width": 480,
+        "scale": 6,
+    }
+}
+
+icons = {
+    "assistant": "https://raw.githubusercontent.com/sahirmaharaj/exifa/2f685de7dffb583f2b2a89cb8ee8bc27bf5b1a40/img/assistant-done.svg",
+    "user": "https://raw.githubusercontent.com/sahirmaharaj/exifa/2f685de7dffb583f2b2a89cb8ee8bc27bf5b1a40/img/user-done.svg",
+}
+
+particles_js = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Particles.js</title>
+  <style>
+  #particles-js {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    z-index: -1; /* Send the animation to the back */
+  }
+  .content {
+    position: relative;
+    z-index: 1;
+    color: white;
+  }
+  
+</style>
+</head>
+<body>
+  <div id="particles-js"></div>
+  <div class="content">
+    <!-- Placeholder for Streamlit content -->
+  </div>
+  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  <script>
+    particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 300,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.2,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 2,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 40,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 100,
+          "color": "#ffffff",
+          "opacity": 0.22,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 0.2,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": true,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "grab"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 100,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 2,
+            "duration": 2,
+            "opacity": 0.5,
+            "speed": 1
+          },
+          "repulse": {
+            "distance": 200,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 2
+          },
+          "remove": {
+            "particles_nb": 3
+          }
+        }
+      },
+      "retina_detect": true
+    });
+  </script>
+</body>
+</html>
+"""
+
+st.set_page_config(page_title="Lequangtai", page_icon="🔥", layout="wide")
+
+
+if "has_snowed" not in st.session_state:
+    st.snow()
+    st.session_state["has_snowed"] = True
+# show html effect
+components.html(particles_js, height=370, scrolling=False)
+
+st.title('Welcome')
